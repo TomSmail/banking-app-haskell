54c12c379e750a9d6ea80fad14eb2f6a326b706d
TomSmail <thomas.c.smail@gmail.com>
2022-09-19 17:30:27 +0100

    built executables

---
+++ llvmFix.sh
@@ -0,0 +1,4 @@
+export LDFLAGS="-L/opt/homebrew/opt/llvm@12/lib"
+export CPPFLAGS="-I/opt/homebrew/opt/llvm@12/include"
+export PATH="/opt/homebrew/opt/llvm@12/bin:$PATH"
+echo 'PATHs have been changed for llvm'
